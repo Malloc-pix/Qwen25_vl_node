707020dcdaf09e71830f3986fffdf7649bba53e2
Malloc-pix <mengdie.feng@pixocial.com>
2025-06-20 09:41:02 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,9 +1,11 @@
-from .qwen25_caption import Qwen25Captioner
+from .qwen25_caption import Qwen25Captioner,CLIPDynamicTextEncode
 
 NODE_CLASS_MAPPINGS = {
     "Qwen25Captioner": Qwen25Captioner,
+    "CLIPDynamicTextEncode": CLIPDynamicTextEncode
 }
 NODE_DISPLAY_NAME_MAPPINGS = {
     "Qwen25Captioner": "Qwen-2.5 Captioner",
+    "CLIPDynamicTextEncode": "CLIP Dynamic Text Encode(cy)",
 }
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
